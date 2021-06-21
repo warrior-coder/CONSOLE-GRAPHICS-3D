ccb8d32b16f64b482856d7c135d4858befc666fd
warrior-coder <gudkou2003@gmail.com>
2021-06-21 14:43:24 +0300

    Add amination, Put figure data in file

--- Console-Graphics-3d.cpp
+++ Console-Graphics-3d.cpp
@@ -1,169 +1,337 @@
+#include <iostream>
 #include <math.h>
 #include <windows.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
 #define PI 3.14159265358979323846
 
-struct Dot_2D
+using namespace std;
+
+// -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+- FIGURE -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+- 
+struct Dot_2d
 {
     double x;
     double y;
 };
-struct Dot_3D
+struct Dot_3d
 {
     double x;
     double y;
     double z;
 };
-
-const int sw = 100;
-const int sh = 100;
-char screen[sh][sw];
-const int VERTEX_NUMBER = 8;
-Dot_2D FIGURE_2D[VERTEX_NUMBER];
-Dot_3D FIGURE_3D[VERTEX_NUMBER] = {
-    { 25, 25, 25  },
-    { 75, 25, 25  },
-    { 75, 75, 25  },
-    { 25, 75, 25  },
-    { 25, 75, -25 },
-    { 75, 75, -25 },
-    { 75, 25, -25 },
-    { 25, 25, -25 }
+struct Edge
+{
+    int begin;
+    int end;
 };
-Dot_3D CAMERA = { 50, 50, 50 };
+struct Rotate_3d_Speed
+{
+    int x;
+    int y;
+    int z;
+};
+struct Figure_3d
+{
+    int vertex_number;
+    int edge_number;
+    Dot_2d* vertexes_projection;
+    Dot_3d* vertexes;
+    Edge* edges;
+    char dot_edge = (char)249;
+    char dot_vertex = (char)254;
+    Rotate_3d_Speed rotate_speed;
 
-void DrawScreen();
-void Rotate(int, double, double, double);
-void ScreenSetDot(double, double, char);
-void GenerateEdge(Dot_2D, Dot_2D);
-void ClearScreen();
-Dot_2D Convert_3d_to_2d(Dot_3D);
+    void read_from_file(const char* fname)
+    {
+        FILE* fp;
 
+        fopen_s(&fp, fname, "rt");
+        if (fp)
+        {
+            // Read vertexes
+            fscanf_s(fp, "%d", &vertex_number);
+
+            vertexes = new Dot_3d[vertex_number];
+            vertexes_projection = new Dot_2d[vertex_number];
+
+            float vert_x, vert_y, vert_z;
+            for (int i = 0; i < vertex_number; i++)
+            {
+                fscanf_s(fp, "%f %f %f", &vert_x, &vert_y, &vert_z);
+                vertexes[i].x = vert_x;
+                vertexes[i].y = vert_y;
+                vertexes[i].z = vert_z;
+            }
+
+            // Read edges
+            fscanf_s(fp, "%d", &edge_number);
+
+            edges = new Edge[edge_number];
+
+            for (int i = 0; i < edge_number; i++)
+            {
+                fscanf_s(fp, "%d %d", &edges[i].begin, &edges[i].end);
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                fscanf_s(fp, "%d %d %d", &rotate_speed.x, &rotate_speed.y, &rotate_speed.z);
+            }
+
+        }
+        else printf("File open error");
+    }
+
+    void Rotate_Z(Dot_3d& vert, double deg_rot, double rot_x0, double rot_y0)
+    {
+        if (deg_rot == 0) return;
+
+        double deg_res, deg0;
+
+        // Move rotation axis to center
+        vert.x -= rot_x0;
+        vert.y -= rot_y0;
+
+        // Switch to polar coordinate system
+        double r = sqrt(vert.x * vert.x + vert.y * vert.y);
+        if (r)
+        {
+            if (vert.x > 0) deg0 = asin(vert.y / r);
+            else deg0 = PI - asin(vert.y / r);
+            deg_res = deg0 + deg_rot / 180 * PI;
+
+            // Get result coordinates in rectangle system
+            vert.x = r * cos(deg_res);
+            vert.y = r * sin(deg_res);
+        }
+
+        // Move rotation axis from center
+        vert.x += rot_x0;
+        vert.y += rot_y0;
+    }
+
+    void Rotate_Y(Dot_3d& vert, double deg_rot, double rot_x0, double rot_z0)
+    {
+        if (deg_rot == 0) return;
+
+        double deg_res, deg0;
+
+        // Move rotation axis to center
+        vert.x -= rot_x0;
+        vert.z -= rot_z0;
+
+        // Switch to polar coordinate system
+        double r = sqrt(vert.x * vert.x + vert.z * vert.z);
+        if (r)
+        {
+            if (vert.x > 0) deg0 = asin(vert.z / r);
+            else deg0 = PI - asin(vert.z / r);
+            deg_res = deg0 + deg_rot / 180 * PI;
+
+            // Get result coordinates in rectangle system
+            vert.x = r * cos(deg_res);
+            vert.z = r * sin(deg_res);
+        }
+
+        // Move rotation axis from center
+        vert.x += rot_x0;
+        vert.z += rot_z0;
+    }
+
+    void Rotate_X(Dot_3d& vert, double deg_rot, double rot_y0, double rot_z0)
+    {
+        if (deg_rot == 0) return;
+
+        double deg_res, deg0;
+
+        // Move rotation axis to center
+        vert.y -= rot_y0;
+        vert.z -= rot_z0;
+
+        // Switch to polar coordinate system
+        double r = sqrt(vert.y * vert.y + vert.z * vert.z);
+        if (r)
+        {
+            if (vert.y > 0) deg0 = asin(vert.z / r);
+            else deg0 = PI - asin(vert.z / r);
+            deg_res = deg0 + deg_rot / 180 * PI;
+
+            // Get result coordinates in rectangle system
+            vert.y = r * cos(deg_res);
+            vert.z = r * sin(deg_res);
+        }
+
+        // Move rotation axis from center
+        vert.y += rot_y0;
+        vert.z += rot_z0;
+    }
+
+    void ratate_3d(int rot_x = 50, int rot_y = 50, int rot_z = 0)
+    {
+        for (int i = 0; i < vertex_number; i++)
+        {
+            Rotate_X(vertexes[i], rotate_speed.x, rot_y, rot_z);
+            Rotate_Y(vertexes[i], rotate_speed.y, rot_x, rot_z);
+            Rotate_Z(vertexes[i], rotate_speed.z, rot_x, rot_y);
+        }
+    }
+
+    void clear()
+    {
+        delete[] vertexes;
+        delete[] vertexes_projection;
+        delete[] edges;
+    }
+};
+
+// -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+- SCREEN -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+struct Console_Screen
+{
+    const int w = 100;
+    const int h = 100;
+    char table[100][101]; // one extra cell for '\0'
+    Dot_3d camera = { 50, 50, 50 };
+
+    void clear()
+    {
+        for (int i = 0; i < w; i++)
+        {
+            for (int j = 0; j < h; j++)
+            {
+                table[i][j] = ' ';
+            }
+            table[i][w] = '\0';
+        }
+    }
+
+    void print()
+    {
+        system("cls");
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                cout << table[i][j];
+            }
+            cout << endl;
+        }
+    }
+
+    void set_dot(double dot_x, double dot_y, char dot)
+    {
+        int sx = round(dot_x);
+        int sy = round(dot_y);
+
+        if (sx >= 0 && sy >= 0 && sx < w && sy < h)
+        {
+            table[sy][sx] = dot;
+        }
+    }
+
+    void generate_edge(Dot_2d vert1, Dot_2d vert2, char edge_dot)
+    {
+        double x, y;
+
+        // Render though X
+        if (vert1.x > vert2.x)
+        {
+            Dot_2d temp = vert1;
+            vert1 = vert2;
+            vert2 = temp;
+        }
+
+        for (x = vert1.x; x < vert2.x; x++)
+        {
+            y = (x - vert1.x) / (vert2.x - vert1.x) * (vert2.y - vert1.y) + vert1.y;
+            set_dot(x, y, edge_dot);
+        }
+
+        // Render though Y
+        if (vert1.y > vert2.y)
+        {
+            Dot_2d temp = vert1;
+            vert1 = vert2;
+            vert2 = temp;
+        }
+
+        for (y = vert1.y; y < vert2.y; y++)
+        {
+            x = (y - vert1.y) / (vert2.y - vert1.y) * (vert2.x - vert1.x) + vert1.x;
+            set_dot(x, y, edge_dot);
+        }
+    }
+
+    Dot_2d convert_dot_3d_to_2d(Dot_3d dot3d)
+    {
+        Dot_2d dot2d;
+        double k;
+
+        if (dot3d.z > 0) k = 1 + dot3d.z / camera.z;
+        else k = 1 / (1 + -dot3d.z / camera.z);
+
+        dot2d.x = camera.x + (dot3d.x - camera.x) * k;
+        dot2d.y = camera.y + (dot3d.y - camera.y) * k;
+
+        return dot2d;
+    }
+
+    void set_figure(Figure_3d FIGURE)
+    {
+        // Projection 3d on 2d SCREEN 
+        for (int i = 0; i < FIGURE.vertex_number; i++)
+        {
+            FIGURE.vertexes_projection[i] = convert_dot_3d_to_2d(FIGURE.vertexes[i]);
+        }
+
+        // Connect vertexes
+        for (int i = 0; i < FIGURE.edge_number; i++)
+        {
+            generate_edge(FIGURE.vertexes_projection[FIGURE.edges[i].begin], FIGURE.vertexes_projection[FIGURE.edges[i].end], FIGURE.dot_edge);
+        }
+
+        // Set vertexes
+        for (int i = 0; i < FIGURE.vertex_number; i++)
+        {
+            set_dot(FIGURE.vertexes_projection[i].x, FIGURE.vertexes_projection[i].y, FIGURE.dot_vertex);
+        }
+    }
+};
+
+
+// -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+- GLOBAL VARS -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+Figure_3d FIGURE_1;
+Console_Screen SCREEN;
+
+// -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+- MAIN -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
 int main()
 {
-    system("mode con cols=100 lines=101");
+    // Console settings
+    char console_size[30] = "mode con cols=";
+    char s[5];
+    _itoa_s(SCREEN.w, s, 10);
+    strcat_s(console_size, s);
+    strcat_s(console_size, " lines=");
+    _itoa_s(SCREEN.h + 1, s, 10);
+    strcat_s(console_size, s);
+    system(console_size);
     system("title Figures 3d");
-    ClearScreen();
+    system("color 0F");
 
-    // Project 3d dots on 2d screen
-    for (int i = 0; i < VERTEX_NUMBER; i++)
-    {
-        FIGURE_2D[i] = Convert_3d_to_2d(FIGURE_3D[i]);
-    }
- 
-    // Connect vertexes
-    GenerateEdge(FIGURE_2D[0], FIGURE_2D[1]);
-    GenerateEdge(FIGURE_2D[1], FIGURE_2D[2]);
-    GenerateEdge(FIGURE_2D[2], FIGURE_2D[3]);
-    GenerateEdge(FIGURE_2D[3], FIGURE_2D[4]);
-    GenerateEdge(FIGURE_2D[4], FIGURE_2D[5]);
-    GenerateEdge(FIGURE_2D[5], FIGURE_2D[6]);
-    GenerateEdge(FIGURE_2D[6], FIGURE_2D[7]);
-    GenerateEdge(FIGURE_2D[7], FIGURE_2D[0]);
-    GenerateEdge(FIGURE_2D[3], FIGURE_2D[0]);
-    GenerateEdge(FIGURE_2D[4], FIGURE_2D[7]);
-    GenerateEdge(FIGURE_2D[6], FIGURE_2D[1]);
-    GenerateEdge(FIGURE_2D[5], FIGURE_2D[2]);
+    // Read figure data
+    FIGURE_1.read_from_file("figure0.dat");
 
-    // Set vertexes
-    for (int i = 0; i < VERTEX_NUMBER; i++)
+    // Figure drawing
+    while (true)
     {
-        ScreenSetDot(FIGURE_2D[i].x, FIGURE_2D[i].y, 'O');
+        SCREEN.clear();
+        FIGURE_1.ratate_3d();
+        SCREEN.set_figure(FIGURE_1);
+        SCREEN.print();
+
+        if (_kbhit()) if (_getch() == 27) break;
     }
 
-    // Draw 
-    DrawScreen();
-
-    getchar();
+    FIGURE_1.clear();
     return 0;
-}
-
-Dot_2D Convert_3d_to_2d(Dot_3D dot3d)
-{
-    Dot_2D dot2d;
-
-    dot2d.x = dot3d.x;
-    dot2d.y = dot3d.y;
-    if (dot3d.z > 0)
-    {
-        double k = 1 + dot3d.z / CAMERA.z;
-        dot2d.x = CAMERA.x + (dot3d.x - CAMERA.x) * k;
-        dot2d.y = CAMERA.y + (dot3d.y - CAMERA.y) * k;
-    }
-    else if (dot3d.z < 0)
-    {
-        double k = 1 + -dot3d.z / CAMERA.z;
-        dot2d.x = CAMERA.x + (dot3d.x - CAMERA.x) / k;
-        dot2d.y = CAMERA.y + (dot3d.y - CAMERA.y) / k;
-    }
-
-    return dot2d;
-}
-
-void ClearScreen()
-{
-    for (int i = 0; i < sw; i++)
-    {
-        for (int j = 0; j < sh; j++)
-        {
-            screen[i][j] = ' ';
-        }
-    }
-}
-
-void DrawScreen()
-{
-    system("cls");
-    for (int i = 0; i < sh; i++)
-    {
-        for (int j = 0; j < sw; j++)
-        {
-            printf("%c", screen[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-void ScreenSetDot(double dot_x, double dot_y, char dot)
-{
-    int sx = floor(dot_x);
-    int sy = floor(dot_y);
-
-    if (sx >= 0 && sy >= 0 && sx < sw && sy < sh)
-    {
-        screen[sy][sx] = dot;
-    }
-}
-
-void GenerateEdge(Dot_2D vert1, Dot_2D vert2)
-{
-    double x, y; 
-
-    // Render though X
-    if (vert1.x > vert2.x)
-    {
-        Dot_2D temp = vert1;
-        vert1 = vert2;
-        vert2 = temp;
-    }
-
-    for (x = vert1.x; x < vert2.x; x++)
-    {
-        y = (x - vert1.x) / (vert2.x - vert1.x) * (vert2.y - vert1.y) + vert1.y;
-        ScreenSetDot(x, y, '.');
-    }
-
-    // Render though Y
-    if (vert1.y > vert2.y)
-    {
-        Dot_2D temp = vert1;
-        vert1 = vert2;
-        vert2 = temp;
-    }
-
-    for (y = vert1.y; y < vert2.y; y++)
-    {
-        x = (y - vert1.y) / (vert2.y - vert1.y) * (vert2.x - vert1.x) + vert1.x;
-        ScreenSetDot(x, y, '.');
-    }
 }
